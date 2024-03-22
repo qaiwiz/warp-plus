576d3e276a7684147a564aeba1ba33579832c766
qaiwiz <123194227+qaiwiz@users.noreply.github.com>
2024-03-21 20:17:26 -0400

    comment

--- ipscanner/internal/ping/ping.go
+++ ipscanner/internal/ping/ping.go
@@ -8,12 +8,15 @@ import (
 	"github.com/bepass-org/warp-plus/ipscanner/internal/statute"
 )
 
+// Ping struct represents a ping object with Options.
 type Ping struct {
 	Options *statute.ScannerOptions
 }
 
-// DoPing performs a ping on the given IP address.
+// DoPing performs a ping on the given IP address using the selected ping operation(s).
+// It returns IPInfo and error.
 func (p *Ping) DoPing(ip netip.Addr) (statute.IPInfo, error) {
+	// Check if HTTP ping operation is selected.
 	if p.Options.SelectedOps&statute.HTTPPing > 0 {
 		res, err := p.httpPing(ip)
 		if err != nil {
@@ -22,85 +25,8 @@ func (p *Ping) DoPing(ip netip.Addr) (statute.IPInfo, error) {
 
 		return res, nil
 	}
+	// Check if TLS ping operation is selected.
 	if p.Options.SelectedOps&statute.TLSPing > 0 {
 		res, err := p.tlsPing(ip)
 		if err != nil {
-			return statute.IPInfo{}, err
-		}
-
-		return res, nil
-	}
-	if p.Options.SelectedOps&statute.TCPPing > 0 {
-		res, err := p.tcpPing(ip)
-		if err != nil {
-			return statute.IPInfo{}, err
-		}
-
-		return res, nil
-	}
-	if p.Options.SelectedOps&statute.QUICPing > 0 {
-		res, err := p.quicPing(ip)
-		if err != nil {
-			return statute.IPInfo{}, err
-		}
-
-		return res, nil
-	}
-	if p.Options.SelectedOps&statute.WARPPing > 0 {
-		res, err := p.warpPing(ip)
-		if err != nil {
-			return statute.IPInfo{}, err
-		}
-
-		return res, nil
-	}
-
-	return statute.IPInfo{}, errors.New("no ping operation selected")
-}
-
-func (p *Ping) httpPing(ip netip.Addr) (statute.IPInfo, error) {
-	return p.calc(
-		NewHttpPing(
-			ip,
-			"GET",
-			fmt.Sprintf(
-				"https://%s:%d%s",
-				p.Options.Hostname,
-				p.Options.Port,
-				p.Options.HTTPPath,
-			),
-			p.Options,
-		),
-	)
-}
-
-func (p *Ping) warpPing(ip netip.Addr) (statute.IPInfo, error) {
-	return p.calc(NewWarpPing(ip, p.Options))
-}
-
-func (p *Ping) tlsPing(ip netip.Addr) (statute.IPInfo, error) {
-	return p.calc(
-		NewTlsPing(ip, p.Options.Hostname, p.Options.Port, p.Options),
-	)
-}
-
-func (p *Ping) tcpPing(ip netip.Addr) (statute.IPInfo, error) {
-	return p.calc(
-		NewTcpPing(ip, p.Options.Hostname, p.Options.Port, p.Options),
-	)
-}
-
-func (p *Ping) quicPing(ip netip.Addr) (statute.IPInfo, error) {
-	return p.calc(
-		NewQuicPing(ip, p.Options.Hostname, p.Options.Port, p.Options),
-	)
-}
-
-func (p *Ping) calc(tp statute.IPing) (statute.IPInfo, error) {
-	pr := tp.Ping()
-	err := pr.Error()
-	if err != nil {
-		return statute.IPInfo{}, err
-	}
-	return pr.Result(), nil
-}
+		
