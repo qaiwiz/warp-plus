3a0bf5403c32cd831a28525c711a7dbd54a3bca7
qaiwiz <123194227+qaiwiz@users.noreply.github.com>
2024-03-21 20:17:53 -0400

    comment

--- ipscanner/example/cfscanner/main.go
+++ ipscanner/example/cfscanner/main.go
@@ -2,20 +2,23 @@ package main
 
 import (
 	"context"
-
-	"github.com/bepass-org/warp-plus/ipscanner"
+	"github.com/bepass-org/warp-plus/ipscanner" // Import the ipscanner package
 )
 
 func main() {
-	// new scanner
+	// Create a new IP scanner using the ipscanner.NewScanner function
 	scanner := ipscanner.NewScanner(
-		ipscanner.WithHTTPPing(),
-		ipscanner.WithUseIPv6(true),
+		ipscanner.WithHTTPPing(), // Enable HTTP ping (ICMP echo)
+		ipscanner.WithUseIPv6(true), // Enable IPv6 support
 	)
+
+	// Create a new context with cancellation and defer cancellation when the function returns
 	ctx, cancel := context.WithCancel(context.Background())
 	defer cancel()
 
+	// Start the scanner as a goroutine
 	go scanner.Run(ctx)
 
+	// Wait for the context to be done (either due to cancellation or an error)
 	<-ctx.Done()
 }
