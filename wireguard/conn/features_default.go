c97c920a474095d1ae02bc5d4419fefeed2a04ac
qaiwiz <123194227+qaiwiz@users.noreply.github.com>
2024-03-21 20:17:59 -0400

    comment

--- wireguard/conn/features_default.go
+++ wireguard/conn/features_default.go
@@ -1,15 +1,9 @@
+// This file contains a function for checking if UDP offloading is supported by a given UDP connection.
+// The file is built only when the operating system is not Linux.
+// The code is licensed under the MIT License.
+
 //go:build !linux
 // +build !linux
 
-/* SPDX-License-Identifier: MIT
- *
- * Copyright (C) 2017-2023 WireGuard LLC. All Rights Reserved.
- */
-
-package conn
-
-import "net"
-
-func supportsUDPOffload(conn *net.UDPConn) (txOffload, rxOffload bool) {
-	return
-}
+// SPDX-License-Identifier: MIT
+// Copyright (C) 2
