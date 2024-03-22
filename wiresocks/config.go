f342627a91affc8b8dbbd03c3009767dc28905e4
qaiwiz <123194227+qaiwiz@users.noreply.github.com>
2024-03-21 20:18:09 -0400

    comment

--- wiresocks/config.go
+++ wiresocks/config.go
@@ -13,29 +13,29 @@ import (
 // PeerConfig struct represents the configuration for a peer in the WireGuard network.
 type PeerConfig struct {
 	// PublicKey is the base64-encoded public key of the peer.
-	PublicKey    string
+	PublicKey    string `ini:"PublicKey"`
 	// PreSharedKey is the base64-encoded pre-shared key of the peer.
-	PreSharedKey string
+	PreSharedKey string `ini:"PreSharedKey"`
 	// Endpoint is the endpoint (IP address and port) of the peer.
-	Endpoint     string
+	Endpoint     string `ini:"Endpoint"`
 	// KeepAlive is the persistent keepalive interval for the peer.
-	KeepAlive    int
+	KeepAlive    int    `ini:"KeepAlive"`
 	// AllowedIPs are the allowed IP addresses for the peer.
-	AllowedIPs   []netip.Prefix
+	AllowedIPs   []netip.Prefix `ini:"AllowedIPs"`
 	// Trick is a flag indicating if the peer should be treated as a local peer.
-	Trick        bool
+	Trick        bool   `ini:"Trick"`
 }
 
 // InterfaceConfig struct represents the configuration for a WireGuard interface.
 type InterfaceConfig struct {
 	// PrivateKey is the base64-encoded private key of the interface.
-	PrivateKey string
+	PrivateKey string `ini:"PrivateKey"`
 	// Addresses are the IP addresses assigned to the interface.
-	Addresses  []netip.Addr
+	Addresses  []netip.Addr `ini:"Address"`
 	// DNS are the DNS servers assigned to the interface.
-	DNS        []netip.Addr
+	DNS        []netip.Addr `ini:"DNS"`
 	// MTU is the Maximum Transmission Unit for the interface.
-	MTU        int
+	MTU        int    `ini:"MTU"`
 }
 
 // Configuration struct represents the overall configuration for the WireGuard network.
@@ -46,99 +46,4 @@ type Configuration struct {
 	Peers     []PeerConfig
 }
 
-// encodeBase64ToHex takes a base64-encoded string and returns its hexadecimal representation.
-func encodeBase64ToHex(key string) (string, error) {
-	// Decode the base64-encoded string.
-	decoded, err := base64.StdEncoding.DecodeString(key)
-	if err != nil {
-		return "", fmt.Errorf("invalid base64 string: %s", key)
-	}
-	// Check if the decoded string is 32 bytes long.
-	if len(decoded) != 32 {
-		return "", fmt.Errorf("key should be 32 bytes: %s", key)
-	}
-	// Encode the decoded string to hexadecimal representation.
-	return hex.EncodeToString(decoded), nil
-}
-
-// ParseInterface parses the [Interface] section of the configuration file and returns the InterfaceConfig.
-func ParseInterface(cfg *ini.File) (InterfaceConfig, error) {
-	// Initialize the InterfaceConfig struct.
-	device := InterfaceConfig{}
-	// Get the [Interface] section of the configuration file.
-	interfaces, err := cfg.SectionsByName("Interface")
-	if len(interfaces) != 1 || err != nil {
-		return InterfaceConfig{}, errors.New("only one [Interface] is expected")
-	}
-	// Get the first (and only) [Interface] section.
-	iface := interfaces[0]
-
-	// Parse the Addresses field.
-	key := iface.Key("Address")
-	if key != nil {
-		var addresses []netip.Addr
-		// Split the string by ',' and parse each IP address.
-		for _, str := range key.StringsWithShadows(",") {
-			prefix, err := netip.ParsePrefix(str)
-			if err != nil {
-				return InterfaceConfig{}, err
-			}
-			addresses = append(addresses, prefix.Addr())
-		}
-		device.Addresses = addresses
-	}
-
-	// Parse the PrivateKey field.
-	key = iface.Key("PrivateKey")
-	if key == nil {
-		return InterfaceConfig{}, errors.New("PrivateKey should not be empty")
-	}
-	// Encode the base64-encoded string to hexadecimal representation.
-	privateKeyHex, err := encodeBase64ToHex(key.String())
-	if err != nil {
-		return InterfaceConfig{}, err
-	}
-	device.PrivateKey = privateKeyHex
-
-	// Parse the DNS field.
-	key = iface.Key("DNS")
-	if key != nil {
-		var addresses []netip.Addr
-		// Split the string by ',' and parse each IP address.
-		for _, str := range key.StringsWithShadows(",") {
-			ip, err := netip.ParseAddr(str)
-			if err != nil {
-				return InterfaceConfig{}, err
-			}
-			addresses = append(addresses, ip)
-		}
-		device.DNS = addresses
-	}
-
-	// Parse the MTU field.
-	if sectionKey, err := iface.GetKey("MTU"); err == nil {
-		value, err := sectionKey.Int()
-		if err != nil {
-			return InterfaceConfig{}, err
-		}
-		device.MTU = value
-	}
-
-	// Return the parsed InterfaceConfig.
-	return device, nil
-}
-
-// ParsePeers parses the [Peer] sections of the configuration file and returns a slice of PeerConfig.
-func ParsePeers(cfg *ini.File) ([]PeerConfig, error) {
-	// Get the [Peer] sections of the configuration file.
-	sections, err := cfg.SectionsByName("Peer")
-	if len(sections) < 1 || err != nil {
-		return nil, errors.New("at least one [Peer] is expected")
-	}
-
-	// Initialize a slice of PeerConfig.
-	peers := make([]PeerConfig, len(sections))
-	// Parse each [Peer] section.
-	for i, section := range sections {
-		peer := PeerConfig{
-			PreSharedKey: "0000000000000000000000000000000000000000
+// encodeBase6
