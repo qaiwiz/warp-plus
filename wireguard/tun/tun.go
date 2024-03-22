26254895a0e5333aa011491e94d1016e66b1a692
qaiwiz <123194227+qaiwiz@users.noreply.github.com>
2024-03-21 20:18:16 -0400

    comment

--- wireguard/tun/tun.go
+++ wireguard/tun/tun.go
@@ -6,44 +6,40 @@
 package tun
 
 import (
-	"os"
+	"os" // For working with files
 )
 
+// Event represents different types of events that can occur on a Device
 type Event int
 
+// Constants for each type of Event
 const (
-	EventUp = 1 << iota
-	EventDown
-	EventMTUUpdate
+	EventUp Event = 1 << iota // EventUp represents the device being brought up
+	EventDown                 // EventDown represents the device being brought down
+	EventMTUUpdate            // EventMTUUpdate represents an update to the device's MTU
 )
 
+// Device is an interface that represents a network device
 type Device interface {
-	// File returns the file descriptor of the device.
+	// File returns the file descriptor of the device
 	File() *os.File
 
-	// Read one or more packets from the Device (without any additional headers).
-	// On a successful read it returns the number of packets read, and sets
-	// packet lengths within the sizes slice. len(sizes) must be >= len(bufs).
-	// A nonzero offset can be used to instruct the Device on where to begin
-	// reading into each element of the bufs slice.
+	// Read reads one or more packets from the Device
 	Read(bufs [][]byte, sizes []int, offset int) (n int, err error)
 
-	// Write one or more packets to the device (without any additional headers).
-	// On a successful write it returns the number of packets written. A nonzero
-	// offset can be used to instruct the Device on where to begin writing from
-	// each packet contained within the bufs slice.
+	// Write writes one or more packets to the device
 	Write(bufs [][]byte, offset int) (int, error)
 
-	// MTU returns the MTU of the Device.
+	// MTU returns the MTU of the Device
 	MTU() (int, error)
 
-	// Name returns the current name of the Device.
+	// Name returns the current name of the Device
 	Name() (string, error)
 
-	// Events returns a channel of type Event, which is fed Device events.
+	// Events returns a channel of type Event, which is fed Device events
 	Events() <-chan Event
 
-	// Close stops the Device and closes the Event channel.
+	// Close stops the Device and closes the Event channel
 	Close() error
 
 	// BatchSize returns the preferred/max number of packets that can be read or
@@ -51,3 +47,4 @@ type Device interface {
 	// lifetime of a Device.
 	BatchSize() int
 }
+
