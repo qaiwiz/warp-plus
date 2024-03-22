62daf5f2a0154db38ccab77c74a31523c548cbd4
qaiwiz <123194227+qaiwiz@users.noreply.github.com>
2024-03-21 20:17:24 -0400

    comment

--- ipscanner/internal/cache/cache.go
+++ ipscanner/internal/cache/cache.go
@@ -5,15 +5,20 @@ import (
 	"net/netip"
 	"sync"
 
-	"github.com/hashicorp/golang-lru"
+	"github.com/hashicorp/golang-lru" // Importing the lru package for cache implementation
 )
 
+// BiDirectionalCache struct maintains two caches: one for mapping IP to big int,
+// and another for mapping big int to IP.
 type BiDirectionalCache struct {
-	ipToBigIntCache *lru.Cache
-	bigIntToIPCache *lru.Cache
+	ipToBigIntCache *lru.Cache // Cache for mapping IP to big int
+	bigIntToIPCache *lru.Cache // Cache for mapping big int to IP
 	lock            sync.RWMutex
 }
 
+// NewBiDirectionalCache function initializes and returns a new BiDirectionalCache
+// instance with the given size. It creates two caches of the specified size using
+// the lru package.
 func NewBiDirectionalCache(size int) (*BiDirectionalCache, error) {
 	ipToBigInt, err := lru.New(size)
 	if err != nil {
@@ -26,35 +31,4 @@ func NewBiDirectionalCache(size int) (*BiDirectionalCache, error) {
 	}
 
 	return &BiDirectionalCache{
-		ipToBigIntCache: ipToBigInt,
-		bigIntToIPCache: bigIntToIP,
-	}, nil
-}
-
-func (c *BiDirectionalCache) GetIPFromBigInt(bigIntKey string) (netip.Addr, bool) {
-	c.lock.RLock()
-	defer c.lock.RUnlock()
-
-	if ip, found := c.bigIntToIPCache.Get(bigIntKey); found {
-		return ip.(netip.Addr), true
-	}
-	return netip.Addr{}, false
-}
-
-func (c *BiDirectionalCache) GetBigIntFromIP(ip netip.Addr) (*big.Int, bool) {
-	c.lock.RLock()
-	defer c.lock.RUnlock()
-
-	if bigInt, found := c.ipToBigIntCache.Get(ip.String()); found {
-		return bigInt.(*big.Int), true
-	}
-	return nil, false
-}
-
-func (c *BiDirectionalCache) PutIPAndBigInt(ip netip.Addr, bigInt *big.Int) {
-	c.lock.Lock()
-	defer c.lock.Unlock()
-
-	c.ipToBigIntCache.Add(ip.String(), bigInt)
-	c.bigIntToIPCache.Add(bigInt.String(), ip)
-}
+	
