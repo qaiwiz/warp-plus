3b972d1f44095b8e122bce0969ee6ccb3bc7f965
qaiwiz <123194227+qaiwiz@users.noreply.github.com>
2024-03-21 20:17:35 -0400

    comment

--- ipscanner/internal/engine/engine.go
+++ ipscanner/internal/engine/engine.go
@@ -11,13 +11,16 @@ import (
 	"github.com/bepass-org/warp-plus/ipscanner/internal/statute"
 )
 
+// Engine represents the scanner engine that generates IP addresses, pings them,
+// and manages the IP queue.
 type Engine struct {
-	generator *iterator.IpGenerator
-	ipQueue   *IPQueue
-	ping      func(netip.Addr) (statute.IPInfo, error)
-	log       *slog.Logger
+	generator *iterator.IpGenerator   // IP address generator
+	ipQueue   *IPQueue                // IP address queue
+	ping      func(netip.Addr) (statute.IPInfo, error) // Ping function to check IP availability
+	log       *slog.Logger             // Logger for the engine
 }
 
+// NewScannerEngine initializes and returns a new ScannerEngine instance.
 func NewScannerEngine(opts *statute.ScannerOptions) *Engine {
 	queue := NewIPQueue(opts)
 
@@ -32,6 +35,7 @@ func NewScannerEngine(opts *statute.ScannerOptions) *Engine {
 	}
 }
 
+// GetAvailableIPs returns a slice of available IPInfo instances based on the engine's IP queue.
 func (e *Engine) GetAvailableIPs(desc bool) []statute.IPInfo {
 	if e.ipQueue != nil {
 		return e.ipQueue.AvailableIPs(desc)
@@ -39,38 +43,11 @@ func (e *Engine) GetAvailableIPs(desc bool) []statute.IPInfo {
 	return nil
 }
 
+// Run is the main function of the engine that runs the IP scanning process in a loop.
 func (e *Engine) Run(ctx context.Context) {
 	for {
 		select {
 		case <-ctx.Done():
 			return
 		case <-e.ipQueue.available:
-			e.log.Debug("Started new scanning round")
-			batch, err := e.generator.NextBatch()
-			if err != nil {
-				e.log.Error("Error while generating IP: %v", err)
-				// in case of disastrous error, to prevent resource draining wait for 2 seconds and try again
-				time.Sleep(2 * time.Second)
-				continue
-			}
-			for _, ip := range batch {
-				select {
-				case <-ctx.Done():
-					return
-				default:
-					e.log.Debug("pinging IP", "addr", ip)
-					if ipInfo, err := e.ping(ip); err == nil {
-						e.log.Debug("ping success", "addr", ipInfo.AddrPort, "rtt", ipInfo.RTT)
-						e.ipQueue.Enqueue(ipInfo)
-					} else {
-						e.log.Error("ping error", "addr", ip, "error", err)
-					}
-				}
-			}
-		default:
-			e.log.Debug("calling expire")
-			e.ipQueue.Expire()
-			time.Sleep(200 * time.Millisecond)
-		}
-	}
-}
+	
