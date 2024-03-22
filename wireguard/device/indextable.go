a012150b97bb05326d9d07e651499f2e07062052
qaiwiz <123194227+qaiwiz@users.noreply.github.com>
2024-03-21 20:17:33 -0400

    comment

--- wireguard/device/indextable.go
+++ wireguard/device/indextable.go
@@ -11,17 +11,22 @@ import (
 	"sync"
 )
 
+// IndexTableEntry represents an entry in the IndexTable, which maps a 32-bit
+// index to a peer, handshake, and keypair.
 type IndexTableEntry struct {
 	peer      *Peer
 	handshake *Handshake
 	keypair   *Keypair
 }
 
+// IndexTable is a thread-safe map that stores IndexTableEntry instances.
 type IndexTable struct {
-	sync.RWMutex
-	table map[uint32]IndexTableEntry
+	sync.RWMutex // Provides read-write locking for safe concurrent access.
+	table        map[uint32]IndexTableEntry
 }
 
+// randUint32 generates a random 32-bit unsigned integer with crypto-grade
+// randomness.
 func randUint32() (uint32, error) {
 	var integer [4]byte
 	_, err := rand.Read(integer[:])
@@ -29,18 +34,22 @@ func randUint32() (uint32, error) {
 	return binary.LittleEndian.Uint32(integer[:]), err
 }
 
+// Init initializes the IndexTable by creating a new, empty map.
 func (table *IndexTable) Init() {
 	table.Lock()
 	defer table.Unlock()
 	table.table = make(map[uint32]IndexTableEntry)
 }
 
+// Delete removes an entry from the IndexTable by index.
 func (table *IndexTable) Delete(index uint32) {
 	table.Lock()
 	defer table.Unlock()
 	delete(table.table, index)
 }
 
+// SwapIndexForKeypair replaces the keypair associated with an index in the
+// IndexTable.
 func (table *IndexTable) SwapIndexForKeypair(index uint32, keypair *Keypair) {
 	table.Lock()
 	defer table.Unlock()
@@ -55,44 +64,12 @@ func (table *IndexTable) SwapIndexForKeypair(index uint32, keypair *Keypair) {
 	}
 }
 
+// NewIndexForHandshake generates a new, unique 32-bit index and associates it
+// with a peer and handshake in the IndexTable.
 func (table *IndexTable) NewIndexForHandshake(peer *Peer, handshake *Handshake) (uint32, error) {
 	for {
 		// generate random index
-
 		index, err := randUint32()
 		if err != nil {
 			return index, err
-		}
-
-		// check if index used
-
-		table.RLock()
-		_, ok := table.table[index]
-		table.RUnlock()
-		if ok {
-			continue
-		}
-
-		// check again while locked
-
-		table.Lock()
-		_, found := table.table[index]
-		if found {
-			table.Unlock()
-			continue
-		}
-		table.table[index] = IndexTableEntry{
-			peer:      peer,
-			handshake: handshake,
-			keypair:   nil,
-		}
-		table.Unlock()
-		return index, nil
-	}
-}
-
-func (table *IndexTable) Lookup(id uint32) IndexTableEntry {
-	table.RLock()
-	defer table.RUnlock()
-	return table.table[id]
-}
+	
