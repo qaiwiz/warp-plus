881db6700fabbf154f37d15be5d5a0929b90590f
qaiwiz <123194227+qaiwiz@users.noreply.github.com>
2024-03-21 20:17:34 -0400

    comment

--- wireguard/tun/operateonfd.go
+++ wireguard/tun/operateonfd.go
@@ -9,16 +9,14 @@ package tun
 
 import (
 	"fmt"
+	"syscall" // sysconn is used to make system calls
 )
 
+// operateOnFd is a method on NativeTun struct that takes a function (fn) as an argument
+// and applies it to the file descriptor (fd) of the tunFile field.
 func (tun *NativeTun) operateOnFd(fn func(fd uintptr)) {
+	// Create a syscallConn from tunFile
 	sysconn, err := tun.tunFile.SyscallConn()
 	if err != nil {
-		tun.errors <- fmt.Errorf("unable to find sysconn for tunfile: %s", err.Error())
-		return
-	}
-	err = sysconn.Control(fn)
-	if err != nil {
-		tun.errors <- fmt.Errorf("unable to control sysconn for tunfile: %s", err.Error())
-	}
-}
+		// If there is an error, send it to the errors channel
+	
