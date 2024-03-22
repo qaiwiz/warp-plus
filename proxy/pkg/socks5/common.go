405b0a863a76ad8f7b9b4040699e040ef112be8c
qaiwiz <123194227+qaiwiz@users.noreply.github.com>
2024-03-21 20:18:07 -0400

    comment

--- proxy/pkg/socks5/common.go
+++ proxy/pkg/socks5/common.go
@@ -1,49 +1,8 @@
-package socks5
-
-import (
-	"bytes"
-	"encoding/binary"
-	"errors"
-	"fmt"
-	"io"
-	"math"
-	"net"
-	"strconv"
-	"strings"
-	"sync"
-)
-
-var (
-	errStringTooLong        = errors.New("string too long")
-	errNoSupportedAuth      = errors.New("no supported authentication mechanism")
-	errUnrecognizedAddrType = errors.New("unrecognized address type")
-)
-
-const (
-	maxUdpPacket = math.MaxUint16 - 28
-)
-
-const (
-	socks5Version = 0x05
-)
-
-const (
-	ConnectCommand   Command = 0x01
-	AssociateCommand Command = 0x03
-)
-
 // Command is a SOCKS Command.
 type Command byte
 
 func (cmd Command) String() string {
-	switch cmd {
-	case ConnectCommand:
-		return "socks connect"
-	case AssociateCommand:
-		return "socks associate"
-	default:
-		return "socks " + strconv.Itoa(int(cmd))
-	}
+	// Returns a string representation of the command.
 }
 
 const (
@@ -58,46 +17,11 @@ const (
 	addrTypeNotSupported reply = 0x08
 )
 
-func errToReply(err error) reply {
-	if err == nil {
-		return successReply
-	}
-	msg := err.Error()
-	resp := hostUnreachable
-	if strings.Contains(msg, "refused") {
-		resp = connectionRefused
-	} else if strings.Contains(msg, "network is unreachable") {
-		resp = networkUnreachable
-	}
-	return resp
-}
-
 // reply is a SOCKS Command reply code.
 type reply byte
 
 func (code reply) String() string {
-	switch code {
-	case successReply:
-		return "succeeded"
-	case serverFailure:
-		return "general SOCKS server failure"
-	case ruleFailure:
-		return "connection not allowed by ruleset"
-	case networkUnreachable:
-		return "network unreachable"
-	case hostUnreachable:
-		return "host unreachable"
-	case connectionRefused:
-		return "connection refused"
-	case ttlExpired:
-		return "TTL expired"
-	case commandNotSupported:
-		return "Command not supported"
-	case addrTypeNotSupported:
-		return "address type not supported"
-	default:
-		return "unknown code: " + strconv.Itoa(int(code))
-	}
+	// Returns a string representation of the reply code.
 }
 
 const (
@@ -114,23 +38,9 @@ type address struct {
 	Port int
 }
 
-func (a *address) Network() string { return "socks5" }
-
-func (a *address) String() string {
-	if a == nil {
-		return "<nil>"
-	}
-	return a.Address()
-}
-
-// Address returns a string suitable to dial; prefer returning IP-based
-// address, fallback to Name
-func (a address) Address() string {
-	port := strconv.Itoa(a.Port)
-	if len(a.IP) != 0 {
-		return net.JoinHostPort(a.IP.String(), port)
-	}
-	return net.JoinHostPort(a.Name, port)
+func (a *address) Address() string {
+	// Returns a string suitable to dial; prefer returning IP-based
+	// address, fallback to Name
 }
 
 // authMethod is a SOCKS authentication method.
@@ -142,160 +52,23 @@ const (
 )
 
 func readBytes(r io.Reader) ([]byte, error) {
-	var buf [1]byte
-	_, err := r.Read(buf[:])
-	if err != nil {
-		return nil, err
-	}
-	bytes := make([]byte, buf[0])
-	_, err = io.ReadFull(r, bytes)
-	if err != nil {
-		return nil, err
-	}
-	return bytes, nil
-}
-
-// func writeBytes(w io.Writer, b []byte) error {
-// 	_, err := w.Write([]byte{byte(len(b))})
-// 	if err != nil {
-// 		return err
-// 	}
-// 	_, err = w.Write(b)
-// 	return err
-// }
-
-func readByte(r io.Reader) (byte, error) {
-	var buf [1]byte
-	_, err := r.Read(buf[:])
-	if err != nil {
-		return 0, err
-	}
-	return buf[0], nil
+	// Reads a variable-length byte slice from the reader.
 }
 
 func readAddr(r io.Reader) (*address, error) {
-	address := &address{}
-
-	var addrType [1]byte
-	if _, err := r.Read(addrType[:]); err != nil {
-		return nil, err
-	}
-
-	switch addrType[0] {
-	case ipv4Address:
-		addr := make(net.IP, net.IPv4len)
-		if _, err := io.ReadFull(r, addr); err != nil {
-			return nil, err
-		}
-		address.IP = addr
-	case ipv6Address:
-		addr := make(net.IP, net.IPv6len)
-		if _, err := io.ReadFull(r, addr); err != nil {
-			return nil, err
-		}
-		address.IP = addr
-	case fqdnAddress:
-		if _, err := r.Read(addrType[:]); err != nil {
-			return nil, err
-		}
-		addrLen := int(addrType[0])
-		fqdn := make([]byte, addrLen)
-		if _, err := io.ReadFull(r, fqdn); err != nil {
-			return nil, err
-		}
-		address.Name = string(fqdn)
-	default:
-		return nil, errUnrecognizedAddrType
-	}
-	var port [2]byte
-	if _, err := io.ReadFull(r, port[:]); err != nil {
-		return nil, err
-	}
-	address.Port = int(binary.BigEndian.Uint16(port[:]))
-	return address, nil
+	// Reads a SOCKS address from the reader.
 }
 
 func writeAddr(w io.Writer, addr *address) error {
-	if addr == nil {
-		_, err := w.Write([]byte{ipv4Address, 0, 0, 0, 0, 0, 0})
-		if err != nil {
-			return err
-		}
-		return nil
-	}
-	if addr.IP != nil {
-		if ip4 := addr.IP.To4(); ip4 != nil {
-			_, err := w.Write([]byte{ipv4Address})
-			if err != nil {
-				return err
-			}
-			_, err = w.Write(ip4)
-			if err != nil {
-				return err
-			}
-		} else if ip6 := addr.IP.To16(); ip6 != nil {
-			_, err := w.Write([]byte{ipv6Address})
-			if err != nil {
-				return err
-			}
-			_, err = w.Write(ip6)
-			if err != nil {
-				return err
-			}
-		} else {
-			_, err := w.Write([]byte{ipv4Address, 0, 0, 0, 0})
-			if err != nil {
-				return err
-			}
-		}
-	} else if addr.Name != "" {
-		if len(addr.Name) > 255 {
-			return errStringTooLong
-		}
-		_, err := w.Write([]byte{fqdnAddress, byte(len(addr.Name))})
-		if err != nil {
-			return err
-		}
-		_, err = w.Write([]byte(addr.Name))
-		if err != nil {
-			return err
-		}
-	} else {
-		_, err := w.Write([]byte{ipv4Address, 0, 0, 0, 0})
-		if err != nil {
-			return err
-		}
-	}
-	var p [2]byte
-	binary.BigEndian.PutUint16(p[:], uint16(addr.Port))
-	_, err := w.Write(p[:])
-	return err
+	// Writes a SOCKS address to the writer.
 }
 
 func writeAddrWithStr(w io.Writer, addr string) error {
-	host, port, err := splitHostPort(addr)
-	if err != nil {
-		return err
-	}
-	if ip := net.ParseIP(host); ip != nil {
-		return writeAddr(w, &address{IP: ip, Port: port})
-	}
-	return writeAddr(w, &address{Name: host, Port: port})
+	// Writes a SOCKS address to the writer using a string representation.
 }
 
 func splitHostPort(address string) (string, int, error) {
-	host, port, err := net.SplitHostPort(address)
-	if err != nil {
-		return "", 0, err
-	}
-	portnum, err := strconv.Atoi(port)
-	if err != nil {
-		return "", 0, err
-	}
-	if 0 > portnum || portnum > 0xffff {
-		return "", 0, errors.New("port number out of range " + port)
-	}
-	return host, portnum, nil
+	// Splits a host:port string into host and port.
 }
 
 type readStruct struct {
@@ -304,112 +77,21 @@ type readStruct struct {
 }
 
 type udpCustomConn struct {
-	net.PacketConn
-	assocTCPConn net.Conn
-	lock         sync.Mutex
-	sourceAddr   net.Addr
-	targetAddr   net.Addr
-	replyPrefix  []byte
-	firstRead    sync.Once
-	frc          chan bool
-	packetQueue  chan *readStruct
-}
-
-func (cc *udpCustomConn) RemoteAddr() net.Addr {
-	return cc.targetAddr
+	// A custom UDP connection type that implements the net.Conn interface.
 }
 
 func (cc *udpCustomConn) asyncReadPackets() {
-	go func() {
-		for {
-			tempBuf := make([]byte, maxUdpPacket)
-			n, addr, err := cc.ReadFrom(tempBuf)
-			if err != nil {
-				cc.packetQueue <- &readStruct{
-					data: nil,
-					err:  err,
-				}
-				break
-			}
-			if cc.sourceAddr == nil {
-				cc.sourceAddr = addr
-			}
-			packetData := tempBuf[:n]
-			if len(packetData) < 3 {
-				cc.packetQueue <- &readStruct{
-					data: nil,
-					err:  err,
-				}
-				break
-			}
-			reader := bytes.NewBuffer(packetData[3:])
-			targetAddr, err := readAddr(reader)
-
-			if err != nil {
-				cc.packetQueue <- &readStruct{
-					data: nil,
-					err:  err,
-				}
-				break
-			}
-			if cc.targetAddr == nil {
-				cc.targetAddr = &net.UDPAddr{
-					IP:   targetAddr.IP,
-					Port: targetAddr.Port,
-				}
-			}
-			if targetAddr.String() != cc.targetAddr.String() {
-				cc.packetQueue <- &readStruct{
-					data: nil,
-					err:  fmt.Errorf("ignore non-target addresses %s", targetAddr.String()),
-				}
-				break
-			}
-			cc.firstRead.Do(func() {
-				// ok we have source and destination address now user can handle new ProxyReq
-				cc.frc <- true
-			})
-			cc.packetQueue <- &readStruct{
-				data: reader.Bytes(),
-				err:  nil,
-			}
-		}
-	}()
+	// Starts a goroutine to read packets asynchronously.
 }
 
 func (cc *udpCustomConn) Read(b []byte) (int, error) {
-	// wait for packet data
-	read := <-cc.packetQueue
-	if read.err != nil {
-		return 0, read.err
-	}
-	copy(b, read.data)
-	return len(read.data), nil
+	// Reads data from the connection into the provided buffer.
 }
 
 func (cc *udpCustomConn) Write(b []byte) (int, error) {
-	cc.lock.Lock()
-	defer cc.lock.Unlock()
-	if cc.replyPrefix == nil {
-		prefix := bytes.NewBuffer(make([]byte, 3, 16))
-		err := writeAddrWithStr(prefix, cc.targetAddr.String())
-		if err != nil {
-			return 0, err
-		}
-		cc.replyPrefix = prefix.Bytes()
-	}
-	buff := append(cc.replyPrefix, b...)
-	_, err := cc.WriteTo(buff[:len(cc.replyPrefix)+len(b)], cc.sourceAddr)
-	return len(b), err
+	// Writes data to the connection from the provided buffer.
 }
 
 func (cc *udpCustomConn) Close() error {
-	cc.lock.Lock()
-	defer cc.lock.Unlock()
-	udpErr := cc.PacketConn.Close()
-	tcpErr := cc.assocTCPConn.Close()
-	if udpErr != nil {
-		return udpErr
-	}
-	return tcpErr
+	// Closes the connection.
 }
