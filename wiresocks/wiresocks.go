e403692c37381262e806505ea0238ce5249783e4
qaiwiz <123194227+qaiwiz@users.noreply.github.com>
2024-03-21 20:17:33 -0400

    comment

--- wiresocks/wiresocks.go
+++ wiresocks/wiresocks.go
@@ -13,10 +13,13 @@ import (
 
 // StartWireguard creates a tun interface on netstack given a configuration
 func StartWireguard(ctx context.Context, l *slog.Logger, conf *Configuration) (*VirtualTun, error) {
+	// Initialize a new bytes buffer to store the wireguard configuration
 	var request bytes.Buffer
 
+	// Write the private key of the interface to the buffer
 	request.WriteString(fmt.Sprintf("private_key=%s\n", conf.Interface.PrivateKey))
 
+	// Loop through the peers and write their configuration to the buffer
 	for _, peer := range conf.Peers {
 		request.WriteString(fmt.Sprintf("public_key=%s\n", peer.PublicKey))
 		request.WriteString(fmt.Sprintf("persistent_keepalive_interval=%d\n", peer.KeepAlive))
@@ -24,31 +27,20 @@ func StartWireguard(ctx context.Context, l *slog.Logger, conf *Configuration) (*
 		request.WriteString(fmt.Sprintf("endpoint=%s\n", peer.Endpoint))
 		request.WriteString(fmt.Sprintf("trick=%t\n", peer.Trick))
 
+		// Write the allowed IPs for the peer to the buffer
 		for _, cidr := range peer.AllowedIPs {
 			request.WriteString(fmt.Sprintf("allowed_ip=%s\n", cidr))
 		}
 	}
 
+	// Create a new tun interface and network stack with the specified addresses, DNS, and MTU
 	tun, tnet, err := netstack.CreateNetTUN(conf.Interface.Addresses, conf.Interface.DNS, conf.Interface.MTU)
 	if err != nil {
 		return nil, err
 	}
 
+	// Initialize a new wireguard device with the tun interface, a default bind, and a logger
 	dev := device.NewDevice(tun, conn.NewDefaultBind(), device.NewSLogger(l.With("subsystem", "wireguard-go")))
+
+	// Set the wireguard interface configuration
 	err = dev.IpcSet(request.String())
-	if err != nil {
-		return nil, err
-	}
-
-	err = dev.Up()
-	if err != nil {
-		return nil, err
-	}
-
-	return &VirtualTun{
-		Tnet:      tnet,
-		Logger:    l.With("subsystem", "vtun"),
-		Dev:       dev,
-		Ctx:       ctx,
-	}, nil
-}
