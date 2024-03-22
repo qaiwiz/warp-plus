9f88dccb408578bc473dbf4ab5802164c301769a
qaiwiz <123194227+qaiwiz@users.noreply.github.com>
2024-03-21 20:17:59 -0400

    comment

--- ipscanner/internal/statute/statute.go
+++ ipscanner/internal/statute/statute.go
@@ -1,6 +1,8 @@
 package statute
 
 import (
+	// Importing necessary packages
+
 	"context"
 	"crypto/tls"
 	"log/slog"
@@ -9,18 +11,23 @@ import (
 	"net/netip"
 	"time"
 
+	// Importing third-party packages
+
 	"github.com/quic-go/quic-go"
 )
 
+// TIPQueueChangeCallback is a type for a function that handles changes in the IP queue
 type TIPQueueChangeCallback func(ips []IPInfo)
 
+// Type definitions
 type (
 	TDialerFunc     func(ctx context.Context, network, addr string) (net.Conn, error)
 	TQuicDialerFunc func(ctx context.Context, addr string, tlsCfg *tls.Config, cfg *quic.Config) (quic.EarlyConnection, error)
 	THTTPClientFunc func(rawDialer TDialerFunc, tlsDialer TDialerFunc, quicDialer TQuicDialerFunc, targetAddr ...string) *http.Client
 )
 
-var (
+// Constants for various network scanning operations
+const (
 	HTTPPing = 1 << 1
 	TLSPing  = 1 << 2
 	TCPPing  = 1 << 3
@@ -28,16 +35,19 @@ var (
 	WARPPing = 1 << 5
 )
 
+// IPInfo struct contains information about an IP address
 type IPInfo struct {
-	AddrPort  netip.AddrPort
-	RTT       time.Duration
-	CreatedAt time.Time
+	AddrPort  netip.AddrPort // Combination of IP address and port number
+	RTT       time.Duration  // Round-trip time
+	CreatedAt time.Time      // Timestamp when the IP was added to the queue
 }
 
+// ScannerOptions struct holds configuration options for the network scanner
 type ScannerOptions struct {
+	// Scanner options
 	UseIPv4               bool
 	UseIPv6               bool
-	CidrList              []netip.Prefix // CIDR ranges to scan
+	CidrList              []netip.Prefix // List of CIDR ranges to scan
 	SelectedOps           int
 	Logger                *slog.Logger
 	InsecureSkipVerify    bool
@@ -58,9 +68,4 @@ type ScannerOptions struct {
 	Port                  uint16
 	IPQueueSize           int
 	IPQueueTTL            time.Duration
-	MaxDesirableRTT       time.Duration
-	IPQueueChangeCallback TIPQueueChangeCallback
-	ConnectionTimeout     time.Duration
-	HandshakeTimeout      time.Duration
-	TlsVersion            uint16
-}
+	MaxDes
