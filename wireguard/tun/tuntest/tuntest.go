ded48488e69e5475bce0eafbeb9a4c33446c5210
qaiwiz <123194227+qaiwiz@users.noreply.github.com>
2024-03-21 20:17:41 -0400

    comment

--- wireguard/tun/tuntest/tuntest.go
+++ wireguard/tun/tuntest/tuntest.go
@@ -2,7 +2,6 @@
  *
  * Copyright (C) 2017-2023 WireGuard LLC. All Rights Reserved.
  */
-
 package tuntest
 
 import (
@@ -11,9 +10,10 @@ import (
 	"net/netip"
 	"os"
 
-	"github.com/bepass-org/warp-plus/wireguard/tun"
+	"github.com/bepass-org/warp-plus/wireguard/tun" // Importing tun package for tun.Event and tun.Device
 )
 
+// Ping generates an ICMPv4 echo request packet for the specified destination and source addresses.
 func Ping(dst, src netip.Addr) []byte {
 	localPort := uint16(1337)
 	seq := uint16(0)
@@ -25,7 +25,7 @@ func Ping(dst, src netip.Addr) []byte {
 	return genICMPv4(payload, dst, src)
 }
 
-// Checksum is the "internet checksum" from https://tools.ietf.org/html/rfc1071.
+// checksum calculates the "internet checksum" as per RFC1071.
 func checksum(buf []byte, initial uint16) uint16 {
 	v := uint32(initial)
 	for i := 0; i < len(buf)-1; i += 2 {
@@ -40,7 +40,9 @@ func checksum(buf []byte, initial uint16) uint16 {
 	return ^uint16(v)
 }
 
+// genICMPv4 generates an ICMPv4 echo request packet with the given payload, destination, and source addresses.
 func genICMPv4(payload []byte, dst, src netip.Addr) []byte {
+	// Constants for ICMPv4 and IPv4 header fields.
 	const (
 		icmpv4ProtocolNumber = 1
 		icmpv4Echo           = 8
@@ -58,13 +60,13 @@ func genICMPv4(payload []byte, dst, src netip.Addr) []byte {
 	ip := pkt[0:ipv4Size]
 	icmpv4 := pkt[ipv4Size : ipv4Size+icmpv4Size]
 
-	// https://tools.ietf.org/html/rfc792
+	// Encoding the ICMPv4 header fields.
 	icmpv4[0] = icmpv4Echo // type
 	icmpv4[1] = 0          // code
 	chksum := ^checksum(icmpv4, checksum(payload, 0))
 	binary.BigEndian.PutUint16(icmpv4[icmpv4ChecksumOffset:], chksum)
 
-	// https://tools.ietf.org/html/rfc760 section 3.1
+	// Encoding the IPv4 header fields.
 	length := uint16(len(pkt))
 	ip[0] = (4 << 4) | (ipv4Size / 4)
 	binary.BigEndian.PutUint16(ip[ipv4TotalLenOffset:], length)
@@ -75,10 +77,12 @@ func genICMPv4(payload []byte, dst, src netip.Addr) []byte {
 	chksum = ^checksum(ip[:], 0)
 	binary.BigEndian.PutUint16(ip[ipv4ChecksumOffset:], chksum)
 
+	// Copying the payload to the packet.
 	copy(pkt[headerSize:], payload)
 	return pkt
 }
 
+// ChannelTUN represents a channel-based TUN device.
 type ChannelTUN struct {
 	Inbound  chan []byte // incoming packets, closed on TUN close
 	Outbound chan []byte // outbound packets, blocks forever on TUN close
@@ -88,6 +92,7 @@ type ChannelTUN struct {
 	tun    chTun
 }
 
+// NewChannelTUN creates a new ChannelTUN instance.
 func NewChannelTUN() *ChannelTUN {
 	c := &ChannelTUN{
 		Inbound:  make(chan []byte),
@@ -100,16 +105,20 @@ func NewChannelTUN() *ChannelTUN {
 	return c
 }
 
+// TUN returns the tun.Device interface for the ChannelTUN.
 func (c *ChannelTUN) TUN() tun.Device {
 	return &c.tun
 }
 
+// chTun is the internal struct for ChannelTUN.tun.
 type chTun struct {
 	c *ChannelTUN
 }
 
+// File returns nil for chTun.
 func (t *chTun) File() *os.File { return nil }
 
+// Read reads packets from the TUN device.
 func (t *chTun) Read(packets [][]byte, sizes []int, offset int) (int, error) {
 	select {
 	case <-t.c.closed:
@@ -122,34 +131,4 @@ func (t *chTun) Read(packets [][]byte, sizes []int, offset int) (int, error) {
 }
 
 // Write is called by the wireguard device to deliver a packet for routing.
-func (t *chTun) Write(packets [][]byte, offset int) (int, error) {
-	if offset == -1 {
-		close(t.c.closed)
-		close(t.c.events)
-		return 0, io.EOF
-	}
-	for i, data := range packets {
-		msg := make([]byte, len(data)-offset)
-		copy(msg, data[offset:])
-		select {
-		case <-t.c.closed:
-			return i, os.ErrClosed
-		case t.c.Inbound <- msg:
-		}
-	}
-	return len(packets), nil
-}
-
-func (t *chTun) BatchSize() int {
-	return 1
-}
-
-const DefaultMTU = 1420
-
-func (t *chTun) MTU() (int, error)        { return DefaultMTU, nil }
-func (t *chTun) Name() (string, error)    { return "loopbackTun1", nil }
-func (t *chTun) Events() <-chan tun.Event { return t.c.events }
-func (t *chTun) Close() error {
-	t.Write(nil, -1)
-	return nil
-}
+func (t *chTun
