7830c6ba195471ca7abcf04de36998706fb7869c
qaiwiz <123194227+qaiwiz@users.noreply.github.com>
2024-03-21 20:17:57 -0400

    comment

--- wireguard/conn/boundif_android.go
+++ wireguard/conn/boundif_android.go
@@ -2,9 +2,11 @@
  *
  * Copyright (C) 2017-2023 WireGuard LLC. All Rights Reserved.
  */
-
 package conn
 
+// PeekLookAtSocketFd4 returns the file descriptor (fd) of the IPv4 socket connection.
+// It first gets a SyscallConn from the IPv4 connection and then uses the Control function
+// to get the file descriptor. If there is an error, it returns -1 and the error.
 func (s *StdNetBind) PeekLookAtSocketFd4() (fd int, err error) {
 	sysconn, err := s.ipv4.SyscallConn()
 	if err != nil {
@@ -14,21 +16,4 @@ func (s *StdNetBind) PeekLookAtSocketFd4() (fd int, err error) {
 		fd = int(f)
 	})
 	if err != nil {
-		return -1, err
-	}
-	return
-}
-
-func (s *StdNetBind) PeekLookAtSocketFd6() (fd int, err error) {
-	sysconn, err := s.ipv6.SyscallConn()
-	if err != nil {
-		return -1, err
-	}
-	err = sysconn.Control(func(f uintptr) {
-		fd = int(f)
-	})
-	if err != nil {
-		return -1, err
-	}
-	return
-}
+	
