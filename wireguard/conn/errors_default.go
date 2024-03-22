60d25fad834609f359b8bd92e69ea3143810acc4
qaiwiz <123194227+qaiwiz@users.noreply.github.com>
2024-03-21 20:17:27 -0400

    comment

--- wireguard/conn/errors_default.go
+++ wireguard/conn/errors_default.go
@@ -1,12 +1,16 @@
-//go:build !linux
+// This Go file contains a single function, errShouldDisableUDPGSO,
+// which takes an error as an input and returns a boolean value.
+// The function is only built for non-Linux systems due to the build constraint: go:build !linux
 
-/* SPDX-License-Identifier: MIT
- *
- * Copyright (C) 2017-2023 WireGuard LLC. All Rights Reserved.
- */
+// SPDX-License-Identifier: MIT
+// This identifies the license for the code that follows, which is the MIT License.
+// The copyright notice indicates that the code is owned by WireGuard LLC.
 
-package conn
+package conn // The package name is conn, which likely contains connection-related functions.
 
+// The function errShouldDisableUDPGSO takes an error (err) as an input and returns
+// a boolean value (bool). It always returns false in this implementation.
 func errShouldDisableUDPGSO(err error) bool {
 	return false
 }
+
