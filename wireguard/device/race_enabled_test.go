12bc61835069d2bae82a8a4a3fc0c90b501e45ab
qaiwiz <123194227+qaiwiz@users.noreply.github.com>
2024-03-21 20:17:29 -0400

    comment

--- wireguard/device/race_enabled_test.go
+++ wireguard/device/race_enabled_test.go
@@ -1,10 +1,10 @@
+// This is a build constraint for the race detector. The code within this
+// block will only be included in the build if the "race" build tag is set.
 //go:build race
 
-/* SPDX-License-Identifier: MIT
- *
- * Copyright (C) 2017-2023 WireGuard LLC. All Rights Reserved.
- */
+// SPDX-License-Identifier: MIT
+// This is a license identifier for the SPDX system, which is used to identify
+// the license of a piece of code. The MIT license is a permissive free software
+// license that originates from the X Window System licenses.
 
-package device
-
-const raceEnabled = true
+// Copyright (C) 2017-2
