9d329445f99d7e01b62c69eba9437ef302f5ecf1
qaiwiz <123194227+qaiwiz@users.noreply.github.com>
2024-03-21 20:17:41 -0400

    comment

--- wireguard/conn/bindtest/bindtest.go
+++ wireguard/conn/bindtest/bindtest.go
@@ -12,29 +12,36 @@ import (
 	"net/netip"
 	"os"
 
-	"github.com/bepass-org/warp-plus/wireguard/conn"
+	"github.com/bepass-org/warp-plus/wireguard/conn" // Importing conn package for implementing Bind and Endpoint interfaces
 )
 
+// ChannelBind struct represents a channel bind object with various channels and endpoints
 type ChannelBind struct {
-	rx4, tx4         *chan []byte
-	rx6, tx6         *chan []byte
-	closeSignal      chan bool
+	rx4, tx4         *chan []byte // Channels for receiving and transmitting IPv4 packets
+	rx6, tx6         *chan []byte // Channels for receiving and transmitting IPv6 packets
+	closeSignal      chan bool   // Channel for signaling closure
 	source4, source6 ChannelEndpoint
 	target4, target6 ChannelEndpoint
 }
 
+// ChannelEndpoint type represents a channel endpoint
 type ChannelEndpoint uint16
 
+// Interfaces are implemented for ChannelBind and ChannelEndpoint types
 var (
-	_ conn.Bind     = (*ChannelBind)(nil)
-	_ conn.Endpoint = (*ChannelEndpoint)(nil)
+	_ conn.Bind     = (*ChannelBind)(nil) // ChannelBind implements Bind interface
+	_ conn.Endpoint = (*ChannelEndpoint)(nil) // ChannelEndpoint implements Endpoint interface
 )
 
+// NewChannelBinds function returns a slice of two ChannelBind objects
 func NewChannelBinds() [2]conn.Bind {
+	// Creating channels for receiving and transmitting packets
 	arx4 := make(chan []byte, 8192)
 	brx4 := make(chan []byte, 8192)
 	arx6 := make(chan []byte, 8192)
 	brx6 := make(chan []byte, 8192)
+
+	// Initializing ChannelBind objects and assigning channels and endpoints
 	var binds [2]ChannelBind
 	binds[0].rx4 = &arx4
 	binds[0].tx4 = &brx4
@@ -52,25 +59,47 @@ func NewChannelBinds() [2]conn.Bind {
 	binds[0].source6 = binds[1].target6
 	binds[1].source4 = binds[0].target4
 	binds[1].source6 = binds[0].target6
+
+	// Returning ChannelBind objects
 	return [2]conn.Bind{&binds[0], &binds[1]}
 }
 
+// ClearSrc method for ChannelEndpoint type
 func (c ChannelEndpoint) ClearSrc() {}
 
+// SrcToString method for ChannelEndpoint type
 func (c ChannelEndpoint) SrcToString() string { return "" }
 
-func (c ChannelEndpoint) DstToString() string { return fmt.Sprintf("127.0.0.1:%d", c) }
+// DstToString method for ChannelEndpoint type
+func (c ChannelEndpoint) DstToString() string {
+	return fmt.Sprintf("127.0.0.1:%d", c) // Returns the endpoint as a string in the format of "127.0.0.1:<port>"
+}
 
-func (c ChannelEndpoint) DstToBytes() []byte { return []byte{byte(c)} }
+// DstToBytes method for ChannelEndpoint type
+func (c ChannelEndpoint) DstToBytes() []byte {
+	return []byte{byte(c)} // Returns the endpoint as a byte slice
+}
 
-func (c ChannelEndpoint) DstIP() netip.Addr { return netip.AddrFrom4([4]byte{127, 0, 0, 1}) }
+// DstIP method for ChannelEndpoint type
+func (c ChannelEndpoint) DstIP() netip.Addr {
+	return netip.AddrFrom4([4]byte{127, 0, 0, 1}) // Returns the endpoint's IP address as a netip.Addr object
+}
 
-func (c ChannelEndpoint) SrcIP() netip.Addr { return netip.Addr{} }
+// SrcIP method for ChannelEndpoint type
+func (c ChannelEndpoint) SrcIP() netip.Addr {
+	return netip.Addr{} // Returns an empty netip.Addr object for the source IP address
+}
 
+// Open method for ChannelBind type
 func (c *ChannelBind) Open(port uint16) (fns []conn.ReceiveFunc, actualPort uint16, err error) {
+	// Initializing closeSignal channel
 	c.closeSignal = make(chan bool)
+
+	// Appending receive functions to the fns slice
 	fns = append(fns, c.makeReceiveFunc(*c.rx4))
 	fns = append(fns, c.makeReceiveFunc(*c.rx6))
+
+	// Generating a random number to determine the actual port
 	if rand.Uint32()&1 == 0 {
 		return fns, uint16(c.source4), nil
 	} else {
@@ -78,59 +107,13 @@ func (c *ChannelBind) Open(port uint16) (fns []conn.ReceiveFunc, actualPort uint
 	}
 }
 
+// Close method for ChannelBind type
 func (c *ChannelBind) Close() error {
+	// Checking if closeSignal channel is initialized
 	if c.closeSignal != nil {
+		// Closing the closeSignal channel
 		select {
 		case <-c.closeSignal:
 		default:
 			close(c.closeSignal)
-		}
-	}
-	return nil
-}
-
-func (c *ChannelBind) BatchSize() int { return 1 }
-
-func (c *ChannelBind) SetMark(mark uint32) error { return nil }
-
-func (c *ChannelBind) makeReceiveFunc(ch chan []byte) conn.ReceiveFunc {
-	return func(bufs [][]byte, sizes []int, eps []conn.Endpoint) (n int, err error) {
-		select {
-		case <-c.closeSignal:
-			return 0, net.ErrClosed
-		case rx := <-ch:
-			copied := copy(bufs[0], rx)
-			sizes[0] = copied
-			eps[0] = c.target6
-			return 1, nil
-		}
-	}
-}
-
-func (c *ChannelBind) Send(bufs [][]byte, ep conn.Endpoint) error {
-	for _, b := range bufs {
-		select {
-		case <-c.closeSignal:
-			return net.ErrClosed
-		default:
-			bc := make([]byte, len(b))
-			copy(bc, b)
-			if ep.(ChannelEndpoint) == c.target4 {
-				*c.tx4 <- bc
-			} else if ep.(ChannelEndpoint) == c.target6 {
-				*c.tx6 <- bc
-			} else {
-				return os.ErrInvalid
-			}
-		}
-	}
-	return nil
-}
-
-func (c *ChannelBind) ParseEndpoint(s string) (conn.Endpoint, error) {
-	addr, err := netip.ParseAddrPort(s)
-	if err != nil {
-		return nil, err
-	}
-	return ChannelEndpoint(addr.Port()), nil
-}
+	
