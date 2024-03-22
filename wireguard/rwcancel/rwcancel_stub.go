f80244ab90c846d5d34b016f115cf46271d0977a
qaiwiz <123194227+qaiwiz@users.noreply.github.com>
2024-03-21 20:18:00 -0400

    comment

--- wireguard/rwcancel/rwcancel_stub.go
+++ wireguard/rwcancel/rwcancel_stub.go
@@ -4,6 +4,10 @@
 
 package rwcancel
 
+// RWCancel struct represents a type that can be used to cancel read-write operations.
 type RWCancel struct{}
 
+// Cancel method cancels any ongoing read-write operations.
+// This method does not return any value.
 func (*RWCancel) Cancel() {}
+
