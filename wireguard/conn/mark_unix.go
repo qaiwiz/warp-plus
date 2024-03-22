b3187d99e29ba328f8eec72466101b8cce678413
qaiwiz <123194227+qaiwiz@users.noreply.github.com>
2024-03-21 20:17:30 -0400

    comment

--- wireguard/conn/mark_unix.go
+++ wireguard/conn/mark_unix.go
@@ -5,16 +5,19 @@
  * Copyright (C) 2017-2023 WireGuard LLC. All Rights Reserved.
  */
 
-package conn
+package conn // The package name is "conn".
 
 import (
 	"runtime"
 
-	"golang.org/x/sys/unix"
+	"golang.org/x/sys/unix" // The "unix" package is imported for system calls.
 )
 
+// fwmarkIoctl is a variable that stores the value of the IOCTL command.
 var fwmarkIoctl int
 
+// init is a special function that is called automatically when the package is imported.
+// It sets the value of fwmarkIoctl based on the operating system.
 func init() {
 	switch runtime.GOOS {
 	case "linux", "android":
@@ -26,40 +29,22 @@ func init() {
 	}
 }
 
+// The "StdNetBind" type is defined here, but its definition is not provided.
+
+// SetMark is a method on the "StdNetBind" type that sets the firewall mark on the socket.
 func (s *StdNetBind) SetMark(mark uint32) error {
 	var operr error
+
+	// If fwmarkIoctl is 0, then the operating system does not support setting the firewall mark,
+	// so the method returns immediately.
 	if fwmarkIoctl == 0 {
 		return nil
 	}
+
+	// The method iterates over the IPv4 and IPv6 sockets and sets the firewall mark on each one.
 	if s.ipv4 != nil {
 		fd, err := s.ipv4.SyscallConn()
 		if err != nil {
 			return err
 		}
-		err = fd.Control(func(fd uintptr) {
-			operr = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, fwmarkIoctl, int(mark))
-		})
-		if err == nil {
-			err = operr
-		}
-		if err != nil {
-			return err
-		}
-	}
-	if s.ipv6 != nil {
-		fd, err := s.ipv6.SyscallConn()
-		if err != nil {
-			return err
-		}
-		err = fd.Control(func(fd uintptr) {
-			operr = unix.SetsockoptInt(int(fd), unix.SOL_SOCKET, fwmarkIoctl, int(mark))
-		})
-		if err == nil {
-			err = operr
-		}
-		if err != nil {
-			return err
-		}
-	}
-	return nil
-}
+		err = fd.Control(
